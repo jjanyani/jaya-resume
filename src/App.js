8d548408916957968754e9814bf0927a9d6d6ef4
Jaya <jayasakharani1592@gmail.com>
2023-12-28 13:49:04 +0530

    Level 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
   AppBar,
   Toolbar,
@@ -10,6 +10,10 @@ import {
   ListItemText,
   ListItemButton,
   Grid,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Parallax } from "react-parallax";
@@ -17,12 +21,11 @@ import CssBaseline from "@mui/material/CssBaseline";
 import img3 from "./images/parallax1.jpg";
 import img1 from "./images/JJ.png";
 import img2 from "./images/Jaya.png";
+import img4 from "./images/hospet.jpg";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
 import Stack from "@mui/material/Stack";
 import { styled, useTheme } from "@mui/material/styles";
 import "./css/rippleanimation.css";
@@ -33,6 +36,22 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import EmailIcon from "@mui/icons-material/Email";
+import PersonIcon from "@mui/icons-material/Person";
+import SubjectIcon from "@mui/icons-material/Subject";
+import MessageIcon from "@mui/icons-material/Message";
+import CardMedia from "@mui/material/CardMedia";
+import { CardActionArea } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+import CloseIcon from "@mui/icons-material/Close";
+import MuiAlert from "@mui/material/Alert";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
+import Fab from "@mui/material/Fab";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -46,8 +65,35 @@ const DrawerHeader = styled("div")(({ theme }) => ({
 function App() {
   const [appBarColor, setAppBarColor] = useState("transparent");
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [snackopen, setSnackOpen] = React.useState(false);
+  const [backOpen, setBackOpen] = React.useState(false);
   const defaultTheme = createTheme();
   const theme = useTheme();
+  const footerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState("hidden");
+
+  // useEffect(() => {
+  //   // Add a scroll event listener
+
+  //   const handleScroll = () => {
+  //     // Set isVisible based on the scroll position
+  //     setIsVisible(window.scrollY > 100); // Adjust the threshold value as needed
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   // Remove the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   useEffect(() => {
     document.title = "Jaya Janyani Resume";
@@ -58,10 +104,12 @@ function App() {
       const scrollPosition = window.scrollY;
       const threshold = 150; // Adjust this value as needed
 
-      if (scrollPosition > threshold) {
+      if (scrollPosition >= threshold) {
         setAppBarColor("black"); // Change to your desired background color
+        setIsVisible("visible");
       } else {
         setAppBarColor("transparent");
+        setIsVisible("hidden");
       }
     };
 
@@ -90,25 +138,136 @@ function App() {
     setDrawerOpen(false);
   };
 
+  const handleSnackClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackOpen(false);
+  };
+
   const handleLinkedInClick = () => {
     window.open("https://www.linkedin.com/in/jaya-janyani-04051561/", "_blank");
   };
 
   const handleFacebookClick = () => {
     window.open("https://www.facebook.com/jaya.sakharani", "_blank");
-  }; 
-  
+  };
+
   const handleInstagramClick = () => {
     window.open("https://www.instagram.com/jayajanyanii/?hl=en", "_blank");
-  }; 
-  
-  
+  };
+
+  const handlePageClick = (page) => {
+    if (page === "Contact" && footerRef.current) {
+      const formPosition = footerRef.current.offsetTop - 60; // Adjust as needed
+      window.scrollTo({ top: formPosition, behavior: "smooth" });
+    } else {
+      // Handle other page clicks if needed
+    }
+  };
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (fieldName, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
+
+  const validate = () => {
+    let valid = true;
+    const newErrors = {};
+
+    // Validate Name
+    if (!formData.name.trim()) {
+      newErrors.name = "Name is required";
+      valid = false;
+    }
+
+    // Validate Email
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+      valid = false;
+    }
+
+    // Validate Subject
+    if (!formData.subject.trim()) {
+      newErrors.subject = "Subject is required";
+      valid = false;
+    }
+
+    // Validate Message
+    if (!formData.message.trim()) {
+      newErrors.message = "Message is required";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleSubmit = async (e) => {
+    if (validate()) {
+      e.preventDefault();
+      setBackOpen(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/send-email", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        } else {
+          // const responseText = await response.text();
+
+          setSnackOpen(true);
+          setBackOpen(false);
+        }
+      } catch (error) {
+        console.error("Error sending email", error);
+      }
+    } else {
+      console.log("Form validation failed");
+    }
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleSnackClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
 
-      <div>
+      <div style={{ backgroundColor: "black" }}>
         <Box sx={{ flexGrow: 1 }}>
           <AppBar
             position="fixed"
@@ -167,7 +326,12 @@ function App() {
                       {pages.map((page) => (
                         <List key={page} sx={{ width: 240 }}>
                           <ListItem disablePadding>
-                            <ListItemButton>
+                            <ListItemButton
+                              onClick={() => {
+                                handlePageClick(page);
+                                setDrawerOpen(false);
+                              }}
+                            >
                               <ListItemText
                                 primary={page}
                                 sx={{ color: "black", fontWeight: "bold" }}
@@ -190,6 +354,7 @@ function App() {
                           display: "block",
                           fontWeight: "bold",
                         }}
+                        onClick={() => handlePageClick(page)}
                       >
                         {page}
                       </Button>
@@ -208,7 +373,7 @@ function App() {
         bgImage={img3}
         strength={300}
         style={{ marginTop: "-64px" }}
-        blur={3}
+        blur={1}
       >
         <div
           style={{
@@ -262,10 +427,29 @@ function App() {
             OCR Consultant, .Net Developer, Website Developer
           </Typography>
           <Stack spacing={2} direction="row" sx={{ marginTop: 4 }}>
-            <ColorButton variant="contained">Hire Me</ColorButton>
+            <ColorButton
+              variant="contained"
+              onClick={() => handlePageClick("Contact")}
+            >
+              Hire Me
+            </ColorButton>
             <ColorButton variant="contained">Download CV</ColorButton>
           </Stack>
         </div>
+        <Fab
+          aria-label="scroll-to-top"
+          onClick={scrollToTop}
+          style={{
+            position: "fixed",
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
+            backgroundColor: "#3B7D23",
+            color: "white",
+            visibility: `${isVisible}`,
+          }}
+        >
+          <KeyboardDoubleArrowUpIcon />
+        </Fab>
       </Parallax>
 
       <Container maxWidth="xl">
@@ -276,6 +460,208 @@ function App() {
           <p>Your regular content goes here.</p>
         </div>
       </Container>
+
+      <div ref={footerRef} style={{ marginTop: 50 }}>
+        <Parallax bgImage={img3} strength={300} blur={3}>
+          <Typography
+            variant="h4"
+            align="center"
+            style={{ color: "white", marginBottom: 30 }}
+          >
+            Feel free to contact me
+          </Typography>
+          <Grid
+            container
+            spacing={4}
+            justifyContent="center"
+            sx={{ marginBottom: 4 }}
+          >
+            <Grid item xs={12} sm={8} md={5}>
+              <Card sx={{ display: "flex", height: "100%" }}>
+                <CardContent>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      label="Name"
+                      fullWidth
+                      margin="normal"
+                      required
+                      helperText={errors.name}
+                      error={!!errors.name}
+                      sx={{
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "green",
+                          },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            {/* Your icon for Name field, e.g., PersonIcon */}
+                            {/* Replace PersonIcon with your desired icon component */}
+                            <PersonIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      value={formData.name}
+                      onChange={(e) => handleChange("name", e.target.value)}
+                    />
+                    <TextField
+                      label="Email"
+                      fullWidth
+                      margin="normal"
+                      type="email"
+                      required
+                      helperText={errors.email}
+                      error={!!errors.email}
+                      sx={{
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "green",
+                          },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            {/* Your icon for Name field, e.g., PersonIcon */}
+                            {/* Replace PersonIcon with your desired icon component */}
+                            <EmailIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      value={formData.email}
+                      onChange={(e) => handleChange("email", e.target.value)}
+                    />
+                    <TextField
+                      label="Subject"
+                      fullWidth
+                      margin="normal"
+                      required
+                      helperText={errors.subject}
+                      error={!!errors.subject}
+                      sx={{
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "green",
+                          },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            {/* Your icon for Name field, e.g., PersonIcon */}
+                            {/* Replace PersonIcon with your desired icon component */}
+                            <SubjectIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      value={formData.subject}
+                      onChange={(e) => handleChange("subject", e.target.value)}
+                    />
+                    <TextField
+                      label="Message"
+                      fullWidth
+                      margin="normal"
+                      multiline
+                      rows={4}
+                      required
+                      helperText={errors.message}
+                      error={!!errors.message}
+                      sx={{
+                        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                          {
+                            borderColor: "green",
+                          },
+                      }}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            {/* Your icon for Name field, e.g., PersonIcon */}
+                            {/* Replace PersonIcon with your desired icon component */}
+                            <MessageIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      value={formData.message}
+                      onChange={(e) => handleChange("message", e.target.value)}
+                    />
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                      <ColorButton variant="contained" type="submit">
+                        Send
+                      </ColorButton>
+                    </div>
+                  </form>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={8} md={5}>
+              <Card sx={{ display: "flex", height: "100%" }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image={img4}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <div>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontWeight: "bold", marginTop: 2 }}
+                      >
+                        Address:
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        House# 29/30, Cake Zone building, Cowlpete, Hosapete -
+                        583201
+                      </Typography>
+
+                      <Typography
+                        variant="h6"
+                        sx={{ fontWeight: "bold", marginTop: 2 }}
+                      >
+                        Mobile:
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        +91 - 8237106387
+                      </Typography>
+
+                      <Typography
+                        variant="h6"
+                        sx={{ fontWeight: "bold", marginTop: 2 }}
+                      >
+                        Email Id:
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        jayajanyani1592@gmail.com
+                      </Typography>
+                    </div>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </Parallax>
+      </div>
+      <Snackbar
+        open={snackopen}
+        autoHideDuration={6000}
+        onClose={handleSnackClose}
+        action={action}
+      >
+        <Alert
+          onClose={handleSnackClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Message sent
+        </Alert>
+      </Snackbar>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backOpen}
+        // onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
 
       <div
         style={{
@@ -312,10 +698,9 @@ function App() {
           >
             <InstagramIcon />
           </IconButton>
-        
         </Stack>
         <p>Jaya Janyani</p>
-        <p>&copy; My Resume. All rights reserved.</p>
+        <p>&copy; My resume. All rights reserved.</p>
       </div>
     </ThemeProvider>
   );
