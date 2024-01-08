868826cccf408de306c041ed46e93c5a3acc8581
Jaya <jayasakharani1592@gmail.com>
2024-01-08 12:36:25 +0530

    Level9

--- src/About.js
+++ src/About.js
@@ -3,7 +3,10 @@ import PropTypes from "prop-types";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
 
 function About(props) {
   const { post } = props;
@@ -12,7 +15,7 @@ function About(props) {
     <Paper
       sx={{
         position: "relative",
-        // backgroundColor: "grey.800",
+        backgroundColor: "grey.800",
         color: "#fff",
         mb: 4,
         backgroundSize: "cover",
@@ -21,201 +24,144 @@ function About(props) {
         backgroundImage: `url(${post.image})`,
       }}
     >
-      {/* Increase the priority of the hero background image */}
-      {
-        <img
-          style={{ display: "none" }}
-          src={post.image}
-          alt={post.imageText}
-        />
-      }
-      <Box
-        sx={{
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          backgroundColor: "rgba(0,0,0,.5)",
-        }}
-      />
-      <Grid container>
-        <Grid item md={6}>
-          <Box
-            sx={{
-              position: "relative",
-              p: { xs: 3, md: 6 },
-              pr: { md: 15 },
-              //   backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              height: "100%",
-            }}
-          >
-            <Typography
-              component="h1"
-              variant="h4"
-              color="inherit"
-              gutterBottom
-              style={{
-                borderBottom: "5px solid #3B7D23",
-                borderRadius: "5px",
-                width: "70%",
+      <Container sx={{ py: 8 }} maxWidth="xl">
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          <Grid item key={1} xs={12} sm={6} md={6}>
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
               }}
             >
-              {post.title}
-            </Typography>
-            <Typography
-              variant="h7"
-              gutterBottom
-              color="inherit"
-              paragraph
-              whiteSpace="pre-line"
-              style={{
-                fontWeight: "bold",
-                marginBottom: 50,
-                textAlign: "justify",
-              }}
-            >
-              {post.description}
-            </Typography>
-            <Typography
-              component="h1"
-              variant="h4"
-              color="inherit"
-              gutterBottom
-              style={{
-                borderBottom: "5px solid #3B7D23",
-                borderRadius: "5px",
-                width: "70%",
-              }}
-            >
-              Strengths
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              whiteSpace="pre-line"
-              style={{ fontWeight: "bold" }}
-            >
-              Work ethic, Analytical, Organizational skills, Creativity, Time
-              Management, Problem Solving, Proactive, Adabtability
-            </Typography>
-            {/* <Link variant="subtitle1" href="#">
-              {post.linkText}
-            </Link> */}
-          </Box>
-        </Grid>
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  color="white"
+                  gutterBottom
+                  style={{
+                    borderBottom: "5px solid #3B7D23",
+                    borderRadius: "5px",
+                    width: "70%",
+                  }}
+                >
+                  {post.title}
+                </Typography>
+                <Typography
+                  variant="h7"
+                  gutterBottom
+                  color="white"
+                  paragraph
+                  whiteSpace="pre-line"
+                  style={{
+                    fontWeight: "bold",
+                    marginBottom: 50,
+                    textAlign: "justify",
+                  }}
+                >
+                  {post.description}
+                </Typography>
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  color="white"
+                  gutterBottom
+                  style={{
+                    borderBottom: "5px solid #3B7D23",
+                    borderRadius: "5px",
+                    width: "70%",
+                  }}
+                >
+                  Strengths
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  whiteSpace="pre-line"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Work ethic, Analytical, Organizational skills, Creativity,
+                  Time Management, Problem Solving, Proactive, Adabtability
+                </Typography>
+              </CardContent>
+              <CardActions></CardActions>
+            </Card>
+          </Grid>
 
-        {/* <Grid item md={6}>
-          <Box
-            sx={{
-              position: "relative",
-              p: { xs: 3, md: 6 },
-              pr: { xs: 15, md: 15 },
-              backgroundColor: "rgba(0, 0, 0, 0.6)",
-              height: "100%",
-              width: "100%",
-            }}
-          >
-            <Typography
-              component="h1"
-              variant="h4"
-              color="inherit"
-              gutterBottom
-              style={{
-                borderBottom: "5px solid #3B7D23",
-                borderRadius: "5px",
-                width: "70%",               
+          <Grid item key={2} xs={12} sm={6} md={6}>
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
               }}
             >
-              Basic Information
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              style={{ fontWeight: "bold", marginBottom: 30 }}
-            >
-              Date of Birth : 15th July, 1992
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              style={{ fontWeight: "bold", marginBottom: 30 }}
-            >
-              Sex : Female
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              style={{ fontWeight: "bold", marginBottom: 30 }}
-            >
-              Marital Status : Married
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              style={{ fontWeight: "bold", marginBottom: 30 }}
-            >
-              Nationality : Indian
-            </Typography>
-            <Typography
-              variant="h7"
-              color="inherit"
-              paragraph
-              whiteSpace="break-spaces"
-              style={{ fontWeight: "bold", marginBottom: 30 }}
-            >
-              Known Languages : English, Hindi, Marathi, Sindhi
-            </Typography>
-          </Box>
-        </Grid> */}
-
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              position: "relative",
-              p: { xs: 3, md: 6 },
-              pr: { xs: 20, md: 20 },
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              height: "100%",
-              width: "100%",
-            }}
-          >
-            <Typography
-              component="h1"
-              variant="h4"
-              color="inherit"
-              gutterBottom
-              style={{
-                borderBottom: "5px solid #3B7D23",
-                borderRadius: "5px",
-              }}
-            >
-              Basic Information
-            </Typography>
-            <Typography
-              variant="h7"
-              gutterBottom
-              color="inherit"
-              paragraph
-              whiteSpace="pre-line"
-              style={{
-                fontWeight: "bold",
-                marginBottom: 50,
-                textAlign: "justify",
-                width:'100%'
-              }}
-            >
-              {"Date of Birth : 15th July, 1992\n\nSex : Female\n\nMarital Status : Married\n\nNationality : Indian\n\nLanguages : English, Hindi, Marathi, Sindhi"}
-            </Typography>
-           
-          </Box>
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  color="white"
+                  gutterBottom
+                  style={{
+                    borderBottom: "5px solid #3B7D23",
+                    borderRadius: "5px",
+                    width: "70%",
+                  }}
+                >
+                  Basic Information
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  style={{ fontWeight: "bold", marginBottom: 30 }}
+                >
+                  Date of Birth : 15th July, 1992
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  style={{ fontWeight: "bold", marginBottom: 30 }}
+                >
+                  Sex : Female
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  style={{ fontWeight: "bold", marginBottom: 30 }}
+                >
+                  Marital Status : Married
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  style={{ fontWeight: "bold", marginBottom: 30 }}
+                >
+                  Nationality : Indian
+                </Typography>
+                <Typography
+                  variant="h7"
+                  color="white"
+                  paragraph
+                  whiteSpace="break-spaces"
+                  style={{ fontWeight: "bold", marginBottom: 30 }}
+                >
+                  Known Languages : English, Hindi, Marathi, Sindhi
+                </Typography>
+              </CardContent>
+              <CardActions></CardActions>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </Paper>
   );
 }
